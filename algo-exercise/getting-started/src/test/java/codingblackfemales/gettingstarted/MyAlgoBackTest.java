1d941f7fc8b500cdb2c75e4ee393758281c1e54f
Rashidat <rawsheedah@yahoo.com>
2024-09-16 12:16:00 +0100

    Add test that checks the behavior of MyAlgoLogic

--- algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoBackTest.java
+++ algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoBackTest.java
@@ -1,7 +1,23 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.algo.AlgoLogic;
+import codingblackfemales.container.Actioner;
+import codingblackfemales.container.AlgoContainer;
+import codingblackfemales.container.RunTrigger;
+import codingblackfemales.sequencer.DefaultSequencer;
+import codingblackfemales.sequencer.Sequencer;
+import codingblackfemales.sequencer.consumer.LoggingConsumer;
+import codingblackfemales.sequencer.net.TestNetwork;
+import codingblackfemales.service.MarketDataService;
+import codingblackfemales.service.OrderService;
+
+import org.agrona.concurrent.UnsafeBuffer;
+import messages.marketdata.*;
+
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.nio.ByteBuffer;
 
 /**
  * This test plugs together all of the infrastructure, including the order book (which you can trade against)
@@ -19,7 +35,92 @@ public class MyAlgoBackTest extends AbstractAlgoBackTest {
 
     @Override
     public AlgoLogic createAlgoLogic() {
-        return new MyAlgoLogic();
+
+        return new MyAlgoLogic(6000, 108.5);
+        // return new MyAlgoLogic();
+    }
+
+
+    private final MessageHeaderEncoder headerEncoder = new MessageHeaderEncoder();
+    private final BookUpdateEncoder encoder = new BookUpdateEncoder();
+
+    private AlgoContainer container;
+
+
+    @Override
+    public Sequencer getSequencer() {
+        final TestNetwork network = new TestNetwork();
+        final Sequencer sequencer = new DefaultSequencer(network);
+
+        final RunTrigger runTrigger = new RunTrigger();
+        final Actioner actioner = new Actioner(sequencer);
+
+        container = new AlgoContainer(new MarketDataService(runTrigger), new OrderService(runTrigger), runTrigger, actioner);
+        //set my algo logic
+        container.setLogic(new MyAlgoLogic(6000, 108.5));
+
+        network.addConsumer(new LoggingConsumer());
+        network.addConsumer(container.getMarketDataService());
+        network.addConsumer(container.getOrderService());
+        network.addConsumer(container);
+
+        return sequencer;
+    }
+
+    private UnsafeBuffer createSampleMarketDataTick(){
+        final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+        final UnsafeBuffer directBuffer = new UnsafeBuffer(byteBuffer);
+
+        //write the encoded output to the direct buffer
+        encoder.wrapAndApplyHeader(directBuffer, 0, headerEncoder);
+
+        //set the fields to desired values
+        encoder.venue(Venue.XLON);
+        encoder.instrumentId(123L);
+        encoder.source(Source.STREAM);
+
+        encoder.bidBookCount(3)
+                .next().price(98L).size(100L)
+                .next().price(95L).size(200L)
+                .next().price(91L).size(300L);
+
+        encoder.askBookCount(4)
+                .next().price(100L).size(101L)
+                .next().price(110L).size(200L)
+                .next().price(115L).size(5000L)
+                .next().price(119L).size(5600L);
+
+        encoder.instrumentStatus(InstrumentStatus.CONTINUOUS);
+
+        return directBuffer;
+    }
+
+    private UnsafeBuffer createSampleMarketDataTick2(){
+        final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+        final UnsafeBuffer directBuffer = new UnsafeBuffer(byteBuffer);
+
+        //write the encoded output to the direct buffer
+        encoder.wrapAndApplyHeader(directBuffer, 0, headerEncoder);
+
+        //set the fields to desired values
+        encoder.venue(Venue.XLON);
+        encoder.instrumentId(123L);
+        encoder.source(Source.STREAM);
+
+        encoder.bidBookCount(3)
+                .next().price(95L).size(100L)
+                .next().price(93L).size(200L)
+                .next().price(91L).size(300L);
+
+        encoder.askBookCount(4)
+                .next().price(98L).size(501L)
+                .next().price(101L).size(200L)
+                .next().price(110L).size(5000L)
+                .next().price(119L).size(5600L);
+
+        encoder.instrumentStatus(InstrumentStatus.CONTINUOUS);
+
+        return directBuffer;
     }
 
     @Test
