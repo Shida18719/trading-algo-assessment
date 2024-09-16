4008e1ef6caf2c57d99babe6350f5d00f3b16310
Rashidat <rawsheedah@yahoo.com>
2024-09-16 12:26:12 +0100

    Add unit test that checks MyAlgoLogic price benchmark

--- algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
+++ algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
@@ -1,7 +1,9 @@
 package codingblackfemales.gettingstarted;
 
 import codingblackfemales.algo.AlgoLogic;
-
+import java.nio.ByteBuffer;
+import messages.marketdata.*;
+import org.agrona.concurrent.UnsafeBuffer;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -18,21 +20,58 @@ import org.junit.Test;
  *
  */
 public class MyAlgoTest extends AbstractAlgoTest {
+    private  MyAlgoLogic algoLogic;
 
     @Override
     public AlgoLogic createAlgoLogic() {
         //this adds your algo logic to the container classes
-        return new MyAlgoLogic();
+        algoLogic = new MyAlgoLogic(6000, 108.5);
+        return algoLogic;
+        
     }
 
+     // Simulate a market data tick that will be passed to the algorithm
+     @Override
+    protected UnsafeBuffer createTick() {
+        // Create and return a properly initialized UnsafeBuffer representing current market data
+        final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+        final UnsafeBuffer directBuffer = new UnsafeBuffer(byteBuffer);
+
+        final MessageHeaderEncoder headerEncoder = new MessageHeaderEncoder();
+        final BookUpdateEncoder encoder = new BookUpdateEncoder();
+
+        // Write the encoded output to the direct buffer
+        encoder.wrapAndApplyHeader(directBuffer, 0, headerEncoder);
+
+        // Set the fields to desired values
+        encoder.venue(Venue.XLON);
+        encoder.instrumentId(123L);
+
+        encoder.askBookCount(3)
+                .next().price(100L).size(101L)
+                .next().price(110L).size(200L)
+                .next().price(115L).size(5000L);
+
+        encoder.bidBookCount(3)
+                .next().price(98L).size(100L)
+                .next().price(95L).size(200L)
+                .next().price(91L).size(300L);
+
+        encoder.instrumentStatus(InstrumentStatus.CONTINUOUS);
+        encoder.source(Source.STREAM);
+
+        return directBuffer;
+    }
+    
 
     @Test
     public void testDispatchThroughSequencer() throws Exception {
+        
 
         //create a sample market data tick....
         send(createTick());
 
         //simple assert to check we had 3 orders created
-        assertEquals(container.getState().getChildOrders().size(), 3);
+        assertEquals(3, container.getState().getChildOrders().size());
     }
 }
