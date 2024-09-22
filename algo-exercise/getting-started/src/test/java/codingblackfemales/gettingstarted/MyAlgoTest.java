66d81af1f1f750c41d0927d2bbd3f08d7d34be98
Rashidat <rawsheedah@yahoo.com>
2024-09-23 00:59:33 +0100

    Testing the hard coded quantity and targetVwap price

--- algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
+++ algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoTest.java
@@ -1,6 +1,9 @@
 package codingblackfemales.gettingstarted;
 
+import codingblackfemales.action.Action;
 import codingblackfemales.algo.AlgoLogic;
+import codingblackfemales.sotw.SimpleAlgoState;
+
 import java.nio.ByteBuffer;
 import messages.marketdata.*;
 import org.agrona.concurrent.UnsafeBuffer;
@@ -20,58 +23,24 @@ import org.junit.Test;
  *
  */
 public class MyAlgoTest extends AbstractAlgoTest {
-    private  MyAlgoLogic algoLogic;
+    // private  MyAlgoLogic algoLogic;
 
     @Override
     public AlgoLogic createAlgoLogic() {
         //this adds your algo logic to the container classes
-        algoLogic = new MyAlgoLogic(6000, 108.5);
-        return algoLogic;
-        
-    }
 
-     // Simulate a market data tick that will be passed to the algorithm
-     @Override
-    protected UnsafeBuffer createTick() {
-        // Create and return a properly initialized UnsafeBuffer representing current market data
-        final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
-        final UnsafeBuffer directBuffer = new UnsafeBuffer(byteBuffer);
-
-        final MessageHeaderEncoder headerEncoder = new MessageHeaderEncoder();
-        final BookUpdateEncoder encoder = new BookUpdateEncoder();
-
-        // Write the encoded output to the direct buffer
-        encoder.wrapAndApplyHeader(directBuffer, 0, headerEncoder);
-
-        // Set the fields to desired values
-        encoder.venue(Venue.XLON);
-        encoder.instrumentId(123L);
-
-        encoder.askBookCount(3)
-                .next().price(100L).size(101L)
-                .next().price(110L).size(200L)
-                .next().price(115L).size(5000L);
-
-        encoder.bidBookCount(3)
-                .next().price(98L).size(100L)
-                .next().price(95L).size(200L)
-                .next().price(91L).size(300L);
-
-        encoder.instrumentStatus(InstrumentStatus.CONTINUOUS);
-        encoder.source(Source.STREAM);
-
-        return directBuffer;
+        return new MyAlgoLogic(13000, 108.5);
     }
     
 
     @Test
     public void testDispatchThroughSequencer() throws Exception {
-        
 
         //create a sample market data tick....
         send(createTick());
-
+        
         //simple assert to check we had 3 orders created
         assertEquals(3, container.getState().getChildOrders().size());
+
     }
 }
