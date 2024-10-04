6df15e21fab075d8f02b2e6e19e5967a219c514f
Rashidat <rawsheedah@yahoo.com>
2024-10-04 16:40:56 +0100

    UI: Add conditional logic to render arrow on the bid and offer side

--- ui-front-end/src/components/market-depth/PriceCell.tsx
+++ ui-front-end/src/components/market-depth/PriceCell.tsx
@@ -1,10 +1,18 @@
 export interface PriceCellProps {
     price: number;
+    side: "bid" | "offer"; // Add a side prop to differentiate between bid and offer
 }
 
 export const PriceCell = (props: PriceCellProps) => {
-    const {price} = props;
+    const {price, side} = props;
+    //Render conditional logic on the side
     return (
-        <td>{price}</td>
-    )
+        <>
+            <td className={side === "bid" ? "bidArrow" : "offerArrow"}>
+                {price}
+                <span>{side === "bid" ? "↑" : "↓"}</span>
+                
+            </td>
+        </>
+    );
 }
