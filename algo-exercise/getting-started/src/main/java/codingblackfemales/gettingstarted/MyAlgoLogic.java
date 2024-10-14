36c28555209d21a2440e1ad53eefff5b4b4280b8
Rashidat <rawsheedah@yahoo.com>
2024-10-14 13:17:55 +0100

    Refactor and add comment after test

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -33,7 +33,8 @@ public class MyAlgoLogic implements AlgoLogic {
         this.targetVWAP = targetVWAP;
     }
 
-    // Checks the current state of the market and makes decisions about whether to place new orders.
+    // Checks the current state of the market and makes decisions about whether to
+    // place new orders.
     @Override
     public Action evaluate(SimpleAlgoState state) {
 
@@ -46,157 +47,203 @@ public class MyAlgoLogic implements AlgoLogic {
         /********
          *
          * Add your logic here....
+         * 
+         * Logic Summary:
+         * Logs the current market state.
+         * Calculate how much trade has been (executedQuantity) 
+         * and determine the remaining quantity to trade (remQuantity).
+         * Limit the number of active child orders to (orderCount - max 5 orders).
+         * Cancel active orders if the price has moved significantly (to avoid poor trades).
+         * Place buy or sell orders based on VWAP and market conditions, 
+         * ensuring not to exceed the remaining quantity.
+         * Use VWAP to help decide if the current market price is favorable for placing orders.
          *
          */
 
-        // Transform each element into long type and sum them up
-
+        // Transform each element into long type and calculates the total quantity 
+        // of shares that have been traded so far, summing up child orders
         executedQuantity = state.getChildOrders().stream().mapToLong(o -> o.getQuantity()).sum();
 
-        double limitOrderPrice = CalculateVWAP(state);
+        long limitOrderPrice = CalculateVWAP(state); // Stores VWAP Method
+        
+        // Calculate the remaining quantity to trade
+        long remQuantity = quantityToTrade - executedQuantity;
 
-        // // Exit condition: stop once the total traded quantity matches the target, since we don't want to over trade
-        if (executedQuantity >= quantityToTrade) {
-            logger.info("[MYALGO] ======VWAP target achieved======. No more orders to place.");
-            return NoAction;
-        }
+        final BidLevel bestBid = state.getBidAt(0);
+        final AskLevel bestAsk = state.getAskAt(0);
 
-        // retrieves a list of child orders, if more than 20, 
+        // retrieves a list of child orders, if Order Creation Limit of 5 is Reached, 
         // prevents the algorithm from creating more orders
         var orderCount = state.getChildOrders().size();
-        if (orderCount > 20) {
+        if (orderCount > 5) {
             return NoAction;
         }
 
-        // Modify method was interfering with order fulfillment
         //checks how many child orders are currently active
         final var activeOrders = state.getActiveChildOrders();
 
-        // Exit condition: stop once the total traded quantity matches the target, since we don't want to over trade
+        // Exit condition: stop if Total Quantity reached, since we don't want to over trade
         if (executedQuantity >= quantityToTrade) {
             logger.info("[MYALGO] Order limit achieved: No more orders to place.");
             return NoAction;
         }
 
+        // Get the count of filled orders from active child orders
+        long filledOrder = state.getActiveChildOrders().stream()
+        .filter(order -> order.getState() == OrderState.FILLED)
+        .count();
+
         // If there are active orders, check conditions before canceling
         if (activeOrders.size() > 0) {
 
-          //Cancel the first active order, if it exists before placing new order:
-          // based on if the price has moved significantly 
-            final var option = activeOrders.stream().findFirst();
+            //Cancel the first active order, if it exists before placing new order:
+            // based on if the price has moved significantly 
+            final var option = activeOrders.stream().findAny();
 
             if (option.isPresent()) {
-                var childOrder = option.get();
+                    var childOrder = option.get();
 
-                double orderPrice = childOrder.getPrice();
+                    double orderPrice = childOrder.getPrice();
+                    var filledOrderQty = childOrder.getFilledQuantity();
+                    double totalFilledQuantity = childOrder.getQuantity();
 
-                // Cancel the order if the price has moved significantly above the price on the top level
-                // Set a threshold for when the price is far enough to warrant cancellation
-                double priceThreshold = 0.05; // 5% threshold
-                var pending = OrderState.PENDING; 
+                    // Cancel the order if the price has moved significantly above the price on the top level
+                    // Set a threshold for when the price is far enough to warrant cancellation
+                    // double priceThreshold = 0.08 & 0.03; // 8%, 5% threshold
 
-                // Check if the order is still pending in order to prevent
-                // cancellation of already filled or partially filled orders.
-                if(childOrder.getState() == pending) {
+                    // Check if the order is still (not filled or partially filled)
 
-                // Calculates the absolute difference between the order price
-                double priceSlide = Math.abs(orderPrice - limitOrderPrice) / limitOrderPrice;
-                    if (priceSlide > priceThreshold) {
+                    if (childOrder.getSide() == Side.BUY) {
+                        double priceThreshold = 0.08;
 
-                    logger.info("[MYLALGO] Cancelling order:" + orderPrice);
-                    return new CancelChildOrder(childOrder);
+                        // Calculate the price slide for a buy order
+                        double priceSlide = Math.abs(orderPrice - limitOrderPrice) / limitOrderPrice;
+                        if (priceSlide > priceThreshold) {
+                            logger.info("[MYALGO] Cancelling BUY ORDER with PRICESLIDE: " + priceSlide);
+                            return new CancelChildOrder(childOrder);
+                        }
+                    } 
+                    else if (childOrder.getSide() == Side.SELL) {
+                        double priceThreshold = 0.03;
+
+                        // Calculate the price slide for a sell order
+                        double priceSlide = Math.abs(limitOrderPrice - orderPrice) / limitOrderPrice;
+                        if (priceSlide > priceThreshold) {
+                            logger.info("[MYALGO] Cancelling SELL ORDER with PRICESLIDE: " + priceSlide);
+                            return new CancelChildOrder(childOrder);
                     }
                 }
-            }
+                
+                else if (childOrder.getState() == filledOrder) {
+                    // Checks case where the order is already filled and ensures not to cancel
+                    logger.info("[MYLALGO] Order is already filled, cannot cancel: " + childOrder);
+                    return NoAction;
+                } 
+                else if (filledOrderQty > 0 && filledOrderQty < totalFilledQuantity) {
+                    // Checks when orders are partially filled
+                    logger.info("[MYLALGO] Partially filled order detected, do not cancel: " + childOrder);
+                    return NoAction;
+                }
+            } 
             else {
                 return NoAction;
             }
         }
 
-        // Calculate the remaining quantity to trade
-        long remQuantity = quantityToTrade - executedQuantity;
-        final BidLevel bestBid = state.getBidAt(0);
-        final AskLevel bestAsk = state.getAskAt(0);
+
+
+        long buyOrder = state.getActiveChildOrders().stream()
+             .filter(order -> order.getSide() == Side.BUY).count();
 
         // Check if we should place a buy order
         // If fewer than 3 child orders exist and there is still more quantity to trade continue
-        if (state.getChildOrders().size() < 3 && remQuantity > 0){
-            // double limitOrderPrice = CalculateVWAP(state);
+        if (buyOrder < 3 && remQuantity > 0){
+            logger.info("[MYALGO] Calculated Limit Order VWAP FOR BUY: " + limitOrderPrice);
 
-            logger.debug("[MYALGO] Calculated VWAP: " + limitOrderPrice);
-
-            // Create a new buy limit order if the calculated VWAP is less than the target VWAP
+            // Create a new buy limit order if the Order Price is less than the target VWAP
             if (limitOrderPrice <= targetVWAP) {
 
                 if (bestBid != null) {
+                    logger.info("[MYALGO] CHECK NULL BID:" + bestBid);
 
-                    // Get bid price at the top of the book
-                    // final BidLevel bestBid = state.getBidAt(0);
-                    long price = bestBid.price; // highest price a buyer is willing to pay
+                        // Get bid price at the top of the book
+                        long price = bestBid.price; // highest price a buyer is willing to pay
 
-                    // Ensures the remaining quantity to trade is the minimum of the best bid quantity
-                    long minQtyToTrade = Math.min(bestBid.quantity, remQuantity);  // The amount left to trade
-                    
+                        // Ensures the remaining quantity to trade is the minimum of the best bid quantity
+                        long minQtyToTrade = Math.min(bestBid.quantity, remQuantity);  // The amount left to trade
+                        
+                        logger.info("[MYALGO] Remaining Quantity: " + remQuantity);
+
                     if(minQtyToTrade > 0) {
-                        logger.info("[MYALGO] Creating a limit BUY order, have:" + state.getChildOrders().size() + " children, want 3, on passive side of book with: " + minQtyToTrade + " @ " + price);
-                        executedQuantity += minQtyToTrade;
-                        return new CreateChildOrder(Side.BUY, minQtyToTrade, price); // Place a buy limit order
+                            logger.info("[MYALGO] Creating a limit BUY order, have:" + state.getChildOrders().size() + " children, want 3, on passive side of book with: " + minQtyToTrade + " @ " + price);
+                            executedQuantity += minQtyToTrade;
+
+                            logger.info("[MYALGO] Calculated PASSIVE EXECUTED QUANTITY: " + executedQuantity);
+
+                            return new CreateChildOrder(Side.BUY, minQtyToTrade, price); // Place a buy limit order
                     }
                     else {
-                        logger.info("[MYALGO] Have:" + state.getChildOrders().size() + " children, want 3, done.");
-                        // Do nothing, when 3 child orders exist
+                            logger.info("[MYALGO] Have:" + state.getChildOrders().size() + " children, want 3, done.");
+                            // Do nothing, when 3 child orders exist
                     }
                 }
             } return NoAction;
+        }
+
+
+        long sellOrder = state.getActiveChildOrders().stream()
+             .filter(order -> order.getSide() == Side.SELL).count();
+
+        if (sellOrder < 3 && remQuantity > 0) {
+            logger.info("[MYALGO] Checking for Sell Orders. Remaining quantity: " + remQuantity);
         
-        }
+            // Set a profit margin threshold, 2% above average buy price
+            double profitThreshold = 0.02;
+            // Sell when the best ask price offer is greater than the profit threshold
 
-        if (state.getChildOrders().size() < 3 && remQuantity > 0){
-            logger.debug("[MYALGO] Calculated VWAP FOR SELL: " + limitOrderPrice);
+            if (bestAsk != null && bestAsk.price > bestBid.price * (1 + profitThreshold)) {
+                long price = bestAsk.price;
+                long minQtyToTrade = Math.min(bestAsk.quantity, remQuantity);
 
-            if (limitOrderPrice >= targetVWAP) {
+                logger.info("[MYALGO] Best Ask: " + bestAsk.price);
 
-                if (bestAsk != null) {
+                logger.info("[MYALGO] SELL ProfitThreshold: " + bestBid.price * (1 + profitThreshold));
+            
+                if (minQtyToTrade > 0) {
 
-                    // Get bid price at the top of the book
-                    // final BidLevel bestBid = state.getBidAt(0);
-                    long price = bestAsk.price; // highest price a buyer is willing to pay
+                        logger.info("[MYALGO] Creating a LIMIT SELL order, have:" + state.getChildOrders().size() + " on Ask side of book with: " + minQtyToTrade + " @ " + price);
+                        logger.info("[MYALGO] Creating a limit SELL order at price: " + price + " for quantity: " + minQtyToTrade);
 
-                    // Ensures the remaining quantity to trade is the minimum of the best bid quantity
-                    long minQtyToTrade = Math.min(bestAsk.quantity, remQuantity);  // The amount left to trade
-                    
-                    if(minQtyToTrade > 0) {
-                        logger.info("[MYALGO] Creating a limit SELL order, have:" + state.getChildOrders().size() + " children, want 3, on passive side of book with: " + minQtyToTrade + " @ " + price);
-                        executedQuantity += minQtyToTrade;
-                        return new CreateChildOrder(Side.SELL, minQtyToTrade, price); // Place a buy limit order
-                    }
-                    else {
-                        logger.info("[MYALGO] Have:" + state.getChildOrders().size() + " children, want 3, done.");
-                        // Do nothing, when 3 child orders exist
-                    }
+                        // Updates the amount of quantity traded when either a buy or sell order is placed.
+                        executedQuantity += minQtyToTrade; 
+                        return new CreateChildOrder(Side.BUY, minQtyToTrade, price); // Place sell order
                 }
+                else {
+                    logger.info("[MYALGO] No sell order placed.");
+                }
+            
             }
-        // }
         }
-         return NoAction;
+        return NoAction;
     }
 
     // Method to calculate VWAP
-    private double CalculateVWAP(SimpleAlgoState state) {
+    private long CalculateVWAP(SimpleAlgoState state) {
         long totalVolume = 0;
         long volumePrice = 0;
-    
+
         // Loop through all child buy orders and sums their prices and quantities
         for (int i = 0; i < state.getChildOrders().size(); i++) {
-            final BidLevel level = state.getBidAt(i);
-            if (level == null) {
-                continue;  // Check for null values
+            final BidLevel bid = state.getBidAt(i);
+            if (bid == null) {
+                continue; // Check for null values
             }
-        // Calculate VWAP from market data (simple average based on bid price and volume)
-        totalVolume += level.quantity;
-        volumePrice += level.price * level.quantity;
+            // Calculate VWAP from market data (Average based on bid price and
+            // volume)
+            totalVolume += bid.quantity;
+            volumePrice += bid.price * bid.quantity;
         }
-
-    return totalVolume > 0 ? (double) volumePrice / totalVolume : 0;
+        return totalVolume > 0 ? (long) volumePrice / totalVolume : 0;
     }
-}            
+
+}
