ca259ffacd84f393d80b4a58cc092f91f3e9324a
Rashidat <rawsheedah@yahoo.com>
2024-10-21 12:07:11 +0100

    Modify test after update, some test moved from MyAlgoTest

--- algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoBackTest.java
+++ algo-exercise/getting-started/src/test/java/codingblackfemales/gettingstarted/MyAlgoBackTest.java
@@ -3,8 +3,8 @@ package codingblackfemales.gettingstarted;
 import codingblackfemales.algo.AlgoLogic;
 import codingblackfemales.sotw.ChildOrder;
 import codingblackfemales.sotw.OrderState;
-import codingblackfemales.action.CancelChildOrder;
 import codingblackfemales.sotw.SimpleAlgoState;
+import messages.order.Side;
 
 import java.util.stream.Collectors;
 import java.util.List;
@@ -12,6 +12,7 @@ import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -32,7 +33,7 @@ public class MyAlgoBackTest extends AbstractAlgoBackTest {
 
 
     private static final long quantityToTrade = 13000L;
-    private static final double targetVWAP = 100L;
+    private static final long targetVWAP = 100L;
 
     @Override
     public AlgoLogic createAlgoLogic() {
@@ -40,14 +41,39 @@ public class MyAlgoBackTest extends AbstractAlgoBackTest {
     }
 
 
+    /**
+     * Tests Summary:
+     * 1. Total Order Creation And FilledQuantity (testTotalOrderCreationAndFilledQuantity):
+     *    - Validates the functionality of the algorithm with market data simulation
+     *    - Asserts check six child orders created
+     *    - Asserts filled quantity matches expected value of 1300 after market movement
+     * 
+     * 2. Order Cancellation (testCancelledOrderCount):
+     *    - Checks exactly 1 order is cancelled under specific market conditions
+     *    - Verifies 5 trades are successfully executed
+     *    - Checks the presence of cancelled orders in active order list
+     * 
+     * 3. Filled Order State (testFilledOrPartialFilledOrders):
+     *    - Asserts filled or partially filled orders should not be cancelled
+     *  
+     * 4. Unfilled Buy Order Cancellation (testCancelUnfilledBuyOrder):
+     *    - Check that unfilled buy orders are cancelled when VWAP exceeds current price
+     * 
+     *  5. VWAP Buy Order Creation (testCalculateVWAPCreateBuyOrder):
+     *    - Asserts buy order creation based on VWAP calculations
+     *    - Ensures buy orders are created only when price is below target VWAP (100)
+     *    - Check limit order price calculation against VWAP benchmark
+     */
+
+
 
     @Test
-    public void testExampleBackTest() throws Exception {
+    public void testTotalOrderCreationAndFilledQuantity() throws Exception {
         //create a sample market data tick....
         send(createTick());
 
-        //ADD asserts when you have implemented your algo logic
-
+        
+        // Asserts total number of child order created
         assertEquals(6, container.getState().getChildOrders().size());
 
         //when: market data moves towards us
@@ -61,7 +87,40 @@ public class MyAlgoBackTest extends AbstractAlgoBackTest {
         //Check things like filled quantity, cancelled order count etc....
         long filledQuantity = state.getChildOrders().stream().map(ChildOrder::getFilledQuantity).reduce(Long::sum).get();
         //and: check that our algo state was updated to reflect our fills when the market data
-        assertEquals(802, filledQuantity); //we should have 802 filled quantity
+        assertEquals(1300, filledQuantity); //we should have 1300 filled quantity
+    }
+
+
+    @Test
+    public void testCancelledOrderCount() throws Exception {
+
+        send(createTick());
+        send(createTick2());
+        send(createTick3());
+        send(createTick4());
+
+        SimpleAlgoState state = container.getState();
+
+        // Count number of cancelled orders
+        long cancelledOrderCount = state.getChildOrders().stream()
+        .filter(order -> order.getState() == OrderState.CANCELLED)
+        .count();
+
+        // Assert check 1 order cancelled
+        assertEquals(1, cancelledOrderCount);
+
+        long nonCancelledOrderCount = state.getChildOrders().stream()
+        .filter(order -> order.getState() != OrderState.CANCELLED)
+        .count();
+
+        // Assert 5 trades Executed
+        assertEquals(5, nonCancelledOrderCount);
+
+        // Check if Any order is cancelled
+        var cancelAnyOrder = state.getActiveChildOrders().stream()
+        .filter(order -> order.getState() == OrderState.CANCELLED).findAny();
+        
+        assertNotNull(cancelAnyOrder);
     }
 
 
@@ -81,11 +140,62 @@ public class MyAlgoBackTest extends AbstractAlgoBackTest {
         .collect(Collectors.toList());
 
         // Assert none of the filled or partial order have been cancelled
-        boolean anyCancelled = filledOrPartialFilledOrders.stream()
+        boolean filledOrPartialOrder = filledOrPartialFilledOrders.stream()
         .anyMatch(order -> order.getState() == OrderState.CANCELLED);
 
         // Assert that none of the filled orders are cancelled
-        assertFalse("Filled orders should not be cancelled", anyCancelled);
+        assertFalse("Filled orders should not be cancelled", filledOrPartialOrder);
+    }
+
+    @Test
+    public void testCancelUnfilledBuyOrder() throws Exception {
+
+        send(createTick());
+        send(createTick2());
+        send(createTick3());
+        send(createTick4());
+
+        SimpleAlgoState state = container.getState();
+
+        // Filter out unfilled orders
+        List<ChildOrder> unFilledOrders = state.getChildOrders().stream()
+        .filter(order -> order.getState() != OrderState.FILLED)
+        .collect(Collectors.toList());
+ 
+        // Assert unfilled orders is cancelled
+        boolean cancelUnfilledBuyOrder = unFilledOrders.stream()
+        .filter(order -> order.getSide() == Side.BUY)
+        .anyMatch(order -> order.getState() == OrderState.CANCELLED);
+
+        
+        assertTrue("Unfilled Buy orders should be cancelled", cancelUnfilledBuyOrder);
+    }
+
+
+    @Test
+    public void testCalculateVWAPCreateBuyOrder() throws Exception {
+
+        send(createTick());
+        send(createTick2());
+        send(createTick3());
+        send(createTick4());
+
+        SimpleAlgoState state = container.getState();
+
+        MyAlgoLogic algoLogic = (MyAlgoLogic) createAlgoLogic();
+
+        // Call the CalculateVWAP method
+        long limitOrderPrice = algoLogic.CalculateVWAP(state);
+        
+        boolean createBuyOrder = state.getActiveChildOrders().stream()
+        .filter(order -> order.getSide() == Side.BUY)
+        .anyMatch(order -> order.getPrice() <= targetVWAP);
+
+        // Simple assert a buy order has been created
+        assertTrue("Create a Buy order if buy the price is less than the targetVWAP benchmark", createBuyOrder);
+
+        // Simple Assert VWAP calculation
+        assertTrue("Create a Buy order with a price close to or above the VWAP (limit order price)", limitOrderPrice <= targetVWAP);
     }
 
 }
